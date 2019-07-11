eebe3be22fd9760e14e5e7012fdad5155c2888b5
Fatima Agheli <fancyfish25@gmail.com>
2019-07-11 11:53:17 -0700

    implement a max working but no optimal stock price solution

--- project1.py
+++ project1.py
@@ -3,10 +3,23 @@
 # if numbers keep going down, dont buy at all,
 
 def get_max_profit(stock_prices):
-  for current in range(len(stock_prices)):
-    print(stock_prices[current])
+  max_profit = 0
 
-  max_profit = 18
+  list_profits = list()
+
+  for currentIndex in range(len(stock_prices) - 1):
+    for currentIndex2 in range (currentIndex + 1, len(stock_prices)):
+      currentProfit = stock_prices[currentIndex2] - stock_prices[currentIndex]
+      list_profits.append(currentProfit)
+
+  max_profit = max(list_profits)
+  
   return max_profit
 
-get_max_profit([55,66,77,44])
+print(get_max_profit([55,66,77,44]))
+# print(get_max_profit([55,66,77,44]))
+
+
+# O(n^2) time complexity, O(n) space complexity
+
+# O(n) in time complexity, O(1) space complexity
