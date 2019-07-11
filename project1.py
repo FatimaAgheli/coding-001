41143ac0e8cdb5f72f5a472c57537227bf4fd81f
Fatima Agheli <fancyfish25@gmail.com>
2019-07-11 11:45:10 -0700

    implement single loop, print stock price

--- project1.py
+++ project1.py
@@ -1,13 +1,12 @@
 # given a list of numbers,
 # find the best time to buy, and the best time to sell,
 # if numbers keep going down, dont buy at all,
+
 def get_max_profit(stock_prices):
   for current in range(len(stock_prices)):
-    # print(stock_prices[current] + ',' + stock_prices[current + 1])
-    print(current)
+    print(stock_prices[current])
 
-  max_profit = 0
+  max_profit = 18
   return max_profit
 
 get_max_profit([55,66,77,44])
-# print(get_max_profit([55,66,77,44]))'
