5e168f4fb7b5ed2f305f3e39b88bbf7025695a4b
Fatima Agheli <fancyfish25@gmail.com>
2019-07-11 12:37:33 -0700

    implement final A+ version of this assignment

--- project1.py
+++ project1.py
@@ -2,16 +2,23 @@
 # find the best time to buy, and the best time to sell,
 # if numbers keep going down, dont buy at all,
 def get_max_profit(stock_prices):
-  max_profit = 0
+# TELL USER they need at least 2 numbers!!!
+  if len(stock_prices) < 2:
+    raise ValueError("need at least 2 numbers!!")
 
-  for currentIndex in range(len(stock_prices) - 1):
-    for currentIndex2 in range (currentIndex + 1, len(stock_prices)):
-      current_profit = stock_prices[currentIndex2] - stock_prices[currentIndex]
-      max_profit = max(current_profit, max_profit)
+  max_profit = stock_prices[1] - stock_prices[0]
+  min_price = stock_prices[0]
   
+  for current_index in range(1, len(stock_prices)):
+    current_price = stock_prices[current_index]
+    profit = current_price - min_price
+    max_profit = max(profit, max_profit)
+    min_price = min(current_price, min_price)
+
   return max_profit
 
-print(get_max_profit([55,66,77,44]))
+# print(get_max_profit([100, 60, 40, 10, 5]))
+print(get_max_profit([5]))
 
 
 # O(n^2) time complexity, O(n) space complexity
