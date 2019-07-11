6baa968755d58fd03e5d93d4bb2ae40b2b9d2788
Fatima Agheli <fancyfish25@gmail.com>
2019-07-11 11:57:02 -0700

    implement working but not optimal solution removal of additional list to save space

--- project1.py
+++ project1.py
@@ -1,23 +1,17 @@
 # given a list of numbers,
 # find the best time to buy, and the best time to sell,
 # if numbers keep going down, dont buy at all,
-
 def get_max_profit(stock_prices):
   max_profit = 0
 
-  list_profits = list()
-
   for currentIndex in range(len(stock_prices) - 1):
     for currentIndex2 in range (currentIndex + 1, len(stock_prices)):
-      currentProfit = stock_prices[currentIndex2] - stock_prices[currentIndex]
-      list_profits.append(currentProfit)
-
-  max_profit = max(list_profits)
+      current_profit = stock_prices[currentIndex2] - stock_prices[currentIndex]
+      max_profit = max(current_profit, max_profit)
   
   return max_profit
 
 print(get_max_profit([55,66,77,44]))
-# print(get_max_profit([55,66,77,44]))
 
 
 # O(n^2) time complexity, O(n) space complexity
